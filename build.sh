a57c2fd03886af28c9fcbadde437e2e50393909a
ajuniper <ajuniper@local.local>
2023-07-07 22:11:12 +0100

    esp8266 support

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-../arduino-cli.exe compile --fqbn esp32:esp32:esp32 --build-property build.partitions=min_spiffs --build-property upload.maximum_size=1966080 -e
+#!/bin/bash
+board=${1:-esp32:esp32:esp32}
+# esp8266:esp8266:nodemcuv2
+echo "Building for $board"
+../arduino-cli.exe compile \
+    --fqbn ${board} \
+    --build-property build.partitions=min_spiffs \
+    --build-property upload.maximum_size=1966080 \
+    -e
