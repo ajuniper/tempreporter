87ab1453e85f0d4208f7c857963d5566115a8e69
ajuniper <ajuniper@local.local>
2023-07-14 22:19:31 +0100

    update to esp format mac

--- set_remap.sh
+++ set_remap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 host="$1"
 [[ -n $host ]] || { echo "Host not given" >&2 ; exit 1 ; }
-curl --fail-with-body  http://$host/remap'?id=28-ff3c01e07679b5&to=boiler.flow'
-curl --fail-with-body  http://$host/remap'?id=28-ff3c01e076b715&to=boiler.rethw'
-curl --fail-with-body  http://$host/remap'?id=28-ff3c01e076ccfc&to=boiler.retmain'
-curl --fail-with-body  http://$host/remap'?id=28-ff3c01e076008c&to=boiler.retextn'
+curl --fail-with-body  http://$host/remap'?id=28-b57976e0013cd3&to=boiler.flow'
+curl --fail-with-body  http://$host/remap'?id=28-15b776e0013c0f&to=boiler.rethw'
+curl --fail-with-body  http://$host/remap'?id=28-fccc76e0013cad&to=boiler.retmain'
+curl --fail-with-body  http://$host/remap'?id=28-8c0076e0013c36&to=boiler.retextn'
 curl --fail-with-body  http://$host/remap'?id=28-ff641e8343e328&to=tank.hw'
 curl --fail-with-body  http://$host/remap'?id=28-ff641e833c7e6a&to=tank.flow'
 curl --fail-with-body  http://$host/remap'?id=28-ff641e83261b3b&to=tank.cold'
