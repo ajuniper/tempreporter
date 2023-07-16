9ae5c92a305de47b0f2d1688aa32c8244a61bc34
ajuniper <ajuniper@local.local>
2023-07-16 13:22:31 +0100

    share build.sh above

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-board=${1:-esp32:esp32:esp32}
-# esp8266:esp8266:nodemcuv2
-echo "Building for $board"
-../arduino-cli.exe compile \
-    --fqbn ${board} \
-    --build-property build.partitions=min_spiffs \
-    --build-property upload.maximum_size=1966080 \
-    -e
